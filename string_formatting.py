19f453fd3378720d7d32001cf26300c164b3b8c8
shivaramsharma <a.shivaram@outlook.com>
2023-06-24 15:01:17 +0530

    Add files via upload

---
+++ string_formatting.py
@@ -0,0 +1,15 @@
+name = "John"
+print("Hello %s!"% name)
+
+age = 23
+print ("%s is %d years old." % (name , age))
+
+mylist = [1,2,3]
+print("A list is %s" % mylist)
+
+#string formatting
+
+data = ("john","Doe", 53.44)
+string_formatting =  "Hello %s %s. your current balance is $%.2f"
+print(string_formatting % data)
+
