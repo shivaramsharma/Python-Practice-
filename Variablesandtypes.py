7be16df8ba1dd63dbbbfaec3a9853beb5c23446d
shivaramsharma <a.shivaram@outlook.com>
2023-06-24 13:51:46 +0530

    Add files via upload

---
+++ Variablesandtypes.py
@@ -0,0 +1,37 @@
+#number
+
+mynum = 7
+print(mynum)
+
+#flaoting
+
+myfloat = 7.2344
+print(myfloat)
+myfloat = float(myfloat)
+print(myfloat)
+
+#string
+a, b = 10,20
+one = 1
+two = 2
+three = one + two
+hello = "hello"
+world = "world"
+helloworld = hello + world
+print(helloworld)
+print(three)
+print(a, b)
+print("Don't ignore anything, work hard and get success")
+
+
+#code 
+mystring = "hello"
+myfloat =  10.0
+myint = 20
+
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("int: %d" % myint)        
