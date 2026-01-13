b6907980f69a26af663f35a57c53391fd657a003
Shivaram Ainavolu <a.shivaram@outlook.com>
2026-01-13 07:39:29 +0530

    day 2 practice

---
+++ dict_merge.py
@@ -0,0 +1,12 @@
+#Create a program to merge two dictionaries.
+
+dict1 = {"name":"shivaram","job":"tcs"}
+dict2 = {"location":"hyderabad","salary":"should improve"}
+dict1.update(dict2)
+
+
+
+
+
+print(dict1)
+print(dict3)
